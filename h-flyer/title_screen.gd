a09d8bc48df1df288e5af6e2c63947b6c07f63cc
Dee <dee.covan@gmail.com>
2025-05-21 00:19:22 +0300

    Keep empty my-golf only to start clean from book

--- h-flyer/title_screen.gd
+++
@@ -1,7 +0,0 @@
-extends Control
-
-func _ready():
-	$Label2.text = "High Score: " + str(Global.high_score)
-
-func _on_texture_button_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
--- h-flyer/ui.gd
+++
@@ -1,7 +0,0 @@
-extends CanvasLayer
-
-func update_fuel(value):
-	$FuelBar.value = value
-	
-func update_score(value):
-	$Score.text = str(value)
